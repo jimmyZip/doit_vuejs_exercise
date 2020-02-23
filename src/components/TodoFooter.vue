dce7f88421fb52531743695ecf9aa69d20b9bf92
jimmyzip <jhkimjimmy@gmail.com>
2020-02-24 02:49:36 +0900

    - 할 일 입력 시 로컬스토리지에는 등록되지만 화면갱신은 바로 안되는 상태까지. - 할일 모두 삭제 시 목록은 바로 갱신 안되는 상태까지

---
+++ src/components/TodoFooter.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="clearAllContainer">
+    <span class="clearAllBtn" @click="clearTodoAll">Clear All</span>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    clearTodoAll() {
+      //로컬스토리지의 목록 지우기
+      localStorage.clear();
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .clearAllContainer {
+    width: 8.5rem;
+    height: 50px;
+    line-height: 50px;
+    background-color: white;
+    border-radius: 5px;
+    margin: 0 auto;
+  }
+  .clearAllBtn {
+    cursor: pointer;
+    color: #e20303;
+    display: block;
+  }
+</style>
---
+++ src/components/TodoInput.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="inputBox shadow">
+    <input type="text" v-model="newTodoItem" placeholder="Type what you have to do" v-on:keypress.enter="addTodo">
+    <span class="addContainer" v-on:click="addTodo">
+      <i class="addBtn fas fa-plus" aria-hidden="true"></i>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTodoItem: '',
+    }
+  },
+  methods: {
+    addTodo() {
+      //todo 입력내역 콘솔 출력
+      //console.log(this.newTodoItem);
+      //todo 입력내역 로컬 스토리지 저장
+      //localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      //exception handling
+      if (this.newTodoItem !== "") {
+        let addedValue = this.newTodoItem && this.newTodoItem.trim();
+        localStorage.setItem(addedValue, addedValue);
+        this.clearInput();
+      }
+    },
+    clearInput() {
+      this.newTodoItem = '';
+    }
+  }
+}
+</script>
+
+<style scoped>
+  input:focus {
+    outline: none;
+  }
+  .inputBox {
+    background: white;
+    height: 50px;
+    line-height: 50px;
+    border-radius: 5px;
+  }
+  .inputBox input {
+    border-style: none;
+    font-size: 0.9rem;
+  }
+  .addContainer {
+    float: right;
+    background: linear-gradient(to right, #6478fb, #8763fb);
+    display: inline-block;
+    width: 3rem;
+    border-radius: 0 5px 5px 0;
+  }
+  .addBtn {
+    color: white;
+    vertical-align: middle;
+  }
+</style>
